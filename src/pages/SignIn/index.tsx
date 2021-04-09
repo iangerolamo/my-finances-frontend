8a732d2ddb8a6079966eb57fc282f9cc66cbec70
iangerolamo <iangerolamo@gmail.com>
2021-04-09 17:10:56 -0300

    Page of Login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
-function SignIn() {
-  return (
-    <div>
-      Login page
-    </div>
-  )
-}
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+const SignIn: React.FC = () => (
+ <Container>
+   <Content>
+     <img src={logoImg} alt="GoFinances" />
+
+     <form>
+       <h1>Faça seu logon</h1>
+       <input placeholder="E-mail" />
+
+       <input type="password" placeholder="Senha" />
+
+       <button type="submit">Entrar</button>
+
+       <a href="forgot">Esqueci minha senha</a>
+     </form>
+
+     <a href="criar">
+       <FiLogIn />
+       Criar conta
+     </a>
+
+   </Content>
+   <Background />
+ </Container>
+);
 
 export default SignIn;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  *{
+  * {
     margin: 0;
     padding: 0;
-    outline: 0;
     box-sizing: border-box;
+    outline: 0;
   }
-
   body {
-    background: #F0F2F5;
-    -webkit-font-smoothing: antialiased
+    background: #5636D3;
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
   }
-
   body, input, button {
-    font: 16px "Poppins"
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
   }
-
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
   button {
     cursor: pointer;
   }
