984df4b7f69d6341bf2750129658d7b0432f0307
iangerolamo <iangerolamo@gmail.com>
2021-04-15 16:00:57 -0300

    Setup service

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vh;
+  height: 100%;
+  max-width: 736px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
+
+export const Content = styled.div`
+  width: 457px;
+  height: 350px;
+  background: #5636D3;
+  border-radius: 45px;
+
+`;
